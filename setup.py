8163d3f5663fcdfe4373ca0ccfc0a460c437df10
chaitanyasairam-d <chaitanyasairam.d@gmail.com>
2022-02-27 11:53:18 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,14 @@
+rom setuptools import setup, find_packages
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A sample python package'
+LONG_DESCRIPTION = 'a sample description'
+
+# Setting up
+setup(
+    name="myPackage",
+    version=VERSION,
+    author="chaitanya",
+    packages=find_packages()
+)
